365d03cdf91e30213d6ce4f467ebcf4071c76b22
jeong <jjung4025@naver.com>
2018-12-14 16:02:38 +0900

    Initial commit

---
+++ src/main/java/Algorithm/Problem2490.java
@@ -0,0 +1,39 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Problem2490 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        String[] input = new String[3];
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            input[i] = sc.nextLine();
+            for (int j = 0; j < input[i].length(); j++) {
+                if (input[i].trim().charAt(j) == '0') {
+                    count++;
+                }
+            }
+            switch (count) {
+                case 0:
+                    System.out.println("E");
+                    break;
+                case 1:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("C");
+                    break;
+                case 4:
+                    System.out.println("D");
+                    break;
+            }
+            count = 0;
+        }
+
+    }
+}

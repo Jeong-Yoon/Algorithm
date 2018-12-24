a499d36ce03e545f159d7ebf30d1031ba10a36ec
jeong <jjung4025@naver.com>
2018-12-24 14:24:34 +0900

    알고리즘 연습

---
+++ src/main/java/Algorithm/Problem2577.java
@@ -0,0 +1,63 @@
+package Algorithm;
+
+import java.util.*;
+
+public class Problem2577 {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int x[] = new int[10];
+
+        String num = String.valueOf(A*B*C);
+
+        for (int i = 0; i < num.length(); i++) {
+            switch(num.charAt(i)) {
+
+                case '0':
+                    x[0]++;
+                    break;
+                case '1':
+                    x[1]++;
+                    break;
+                case '2':
+                    x[2]++;
+                    break;
+                case '3':
+                    x[3]++;
+                    break;
+                case '4':
+                    x[4]++;
+                    break;
+                case '5':
+                    x[5]++;
+                    break;
+                case '6':
+                    x[6]++;
+                    break;
+                case '7':
+                    x[7]++;
+                    break;
+                case '8':
+                    x[8]++;
+                    break;
+                case '9':
+                    x[9]++;
+                    break;
+            }
+
+        }
+
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(x[i]);
+        }
+
+    }
+
+}

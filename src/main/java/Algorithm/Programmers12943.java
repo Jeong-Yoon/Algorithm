a7f5877c43f2b0a0849b3a878b2cfb10014bf259
jeong <jjung4025@naver.com>
2018-12-24 15:26:15 +0900

    알고리즘 연습

---
+++ src/main/java/Algorithm/Programmers12943.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+public class Programmers12943 {
+    public static int solution(int num) {
+        int answer = 0;
+        long n = (long) num;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            answer++;
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+
+}
+
+

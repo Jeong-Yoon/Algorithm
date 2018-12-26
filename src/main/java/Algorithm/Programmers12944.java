1449234fe91ee47530672853e0cdb23d15975640
jeong <jjung4025@naver.com>
2018-12-26 11:26:26 +0900

    알고리즘 연습

---
+++ src/main/java/Algorithm/Programmers12944.java
@@ -0,0 +1,11 @@
+package Algorithm;
+
+public class Programmers12944 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer / arr.length;
+    }
+}

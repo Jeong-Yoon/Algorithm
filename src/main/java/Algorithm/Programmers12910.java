ade7278cfea7b34b25589c5230699897d9beb80d
jeong <jjung4025@naver.com>
2018-12-29 22:48:19 +0900

    알고리즘 연습

---
+++ src/main/java/Algorithm/Programmers12910.java
@@ -0,0 +1,36 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Programmers12910 {
+    public static int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[] answer = {};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                arrayList.add(arr[i]);
+            }
+        }
+        if (arrayList.size() == 0) {
+            answer = new int[]{-1};
+        }else {
+            answer = new int[arrayList.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = arrayList.get(i);
+            }
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {3,2,6};
+        solution(arr, 5);
+        for (int i : solution(arr, 5)) {
+            System.out.println(i);
+        }
+    }
+}
+

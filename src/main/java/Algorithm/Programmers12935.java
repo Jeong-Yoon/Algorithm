86ab1f20f4e32fd10de90be70c947c7a0014fc9a
jeong <jjung4025@naver.com>
2018-12-31 10:43:51 +0900

    알고리즘 연습

---
+++ src/main/java/Algorithm/Programmers12935.java
@@ -0,0 +1,25 @@
+package Algorithm;
+
+public class Programmers12935 {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length-1];
+        int tmp = arr[0];
+        int min = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (tmp > arr[i]) {
+                tmp = arr[i];
+                min = i;
+            }
+        }
+        for(int i=0; i < min; i++){
+                    answer[i] = arr[i];
+        }
+        for (int i = min; i < answer.length; i++){
+            answer[i] = arr[i+1];
+        }
+        if (answer.length == 0){
+            answer = new int[]{-1};
+        }
+        return answer;
+    }
+}

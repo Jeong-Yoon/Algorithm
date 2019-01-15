b203bc392fd744e8fd0f78cebf1f51838e872d5a
Jeong-Yoon <45753333+Jeong-Yoon@users.noreply.github.com>
2019-01-15 18:21:35 +0900

    프로그래머스 12933 정수 내림차순으로 배치하기 알고리즘 문제 연습

--- src/main/java/Algorithm/Programmers12933.java
+++ src/main/java/Algorithm/Programmers12933.java
@@ -1,11 +1,30 @@
 package Algorithm;
 
 public class Programmers12933 {
+import java.util.Arrays;
+
+public class Programmers12933 {
     public long solution(long n) {
         long answer = 0;
-        for(int i = 0; i < n; i++){
+        char[] number = Long.toString(n).toCharArray();
 
+        String str = "";
+
+        for (int i = 0; i < number.length; i++){
+            for (int j = i+1; j < number.length; j++){
+                if (number[i] - 48 < number[j] - 48){
+                    char tmp = number[i];
+                    number[i] = number[j];
+                    number[j] = tmp;
+                }
+            }
         }
-            return answer;
+        str = new String(number);
+        answer = Long.parseLong(str);
+
+        return answer;
+    }
+    public static void main(String args[]){
+        System.out.println(solution());
     }
 }

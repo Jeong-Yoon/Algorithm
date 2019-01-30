42ff03102077f2d39176c36365ac1d24c25dffcf
Jeong-Yoon <yj4025@gmail.com>
2019-01-30 16:14:41 +0900

    프로그래머스 12918 문자열 다루기 기본 알고리즘 문제 연습

---
+++ src/main/java/Programmers/Programmers12918.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class Programmers12918 {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() == 4 || s.length() == 6){
+            for(int i = 0; i < s.length(); i++){
+                if (s.charAt(i) > '9'){
+                    answer = false;
+                }
+            }
+        }else {
+            answer = false;
+        }
+        return answer;
+    }
+
+
+    public static void main(String args[]){
+        Programmers12918 p = new Programmers12918();
+        System.out.println(p.solution("1234"));
+    }
+}

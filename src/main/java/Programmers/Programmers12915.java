57c234485af705cc89d4701c2c632924278580aa
Jeong-Yoon <yj4025@gmail.com>
2019-01-31 18:40:31 +0900

    프로그래머스 12915 문자열 내 마음대로 정렬하기 알고리즘 연습

---
+++ src/main/java/Programmers/Programmers12915.java
@@ -0,0 +1,39 @@
+package Programmers;
+
+// 런타임에러
+public class Programmers12915 {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+
+        for (int i = 0; i < strings.length-1; i++){
+            for (int j = i + 1; j < strings.length; j++){
+
+                if (strings[i].charAt(n) > strings[j].charAt(n)){
+                    String tmp = strings[j];
+                    strings[j] = strings[i];
+                    strings[i] = tmp;
+                    break;
+                }else if(strings[i].charAt(n) == strings[j].charAt(n)){
+                    if(strings[i].compareTo(strings[j]) > 0){
+                        String tmp = strings[j];
+                            strings[j] = strings[i];
+                            strings[i] = tmp;
+                    }
+                }
+            }
+        }
+
+        answer = strings;
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers12915 p = new Programmers12915();
+        String[] strings = {"abzcd","cdzab","abzfg","abzaa","abzbb","bbzaa"};
+        p.solution(strings,2);
+
+        for (String s : strings){
+            System.out.println(s);
+        }
+    }
+}

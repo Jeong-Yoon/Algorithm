ebb35c12e46fd09b2d0665339c3dcc06e1a92fe5
Jeong-Yoon <yj4025@gmail.com>
2019-02-06 22:48:41 +0900

    프로그래머스 12906 같은 숫자는 싫어 알고리즘 문제

---
+++ src/main/java/Programmers/Programmers12906.java
@@ -0,0 +1,38 @@
+package Programmers;
+
+import java.util.*;
+
+public class Programmers12906 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int  i = 1; i < arr.length; i++){
+            if (arr[i-1] != arr[i]){
+                list.add(arr[i]);
+            }
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers12906 p = new Programmers12906();
+        int[] n1 = {1,1,3,3,0,1,1};
+        int[] n2 = {4,4,4,3,3};
+
+        int[] ans1 = p.solution(n1);
+        int[] ans2 = p.solution(n2);
+
+        for (int i : ans1){
+            System.out.println(i);
+        }
+
+        for (int i : ans2){
+            System.out.println(i);
+        }
+    }
+}

ca39265e6122bc695edc022123d5f7fef6345217
Jeong-Yoon <yj4025@gmail.com>
2019-02-12 10:19:40 +0900

    프로그래머스 12917 문자열 내림차순으로 배치하기

---
+++ src/main/java/Programmers/Programmers12917.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Programmers12917 {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        Arrays.sort(str);
+        for (int i = str.length-1; i >= 0; i--){
+            answer = answer+str[i];
+        }
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        Programmers12917 p = new Programmers12917();
+        System.out.println(p.solution("Zbcdefg"));
+    }
+}

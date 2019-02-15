b5c5d940cb67fd4f2cb3a3f0f562b6a1704a23be
Jeong-Yoon <yj4025@gmail.com>
2019-02-15 16:01:40 +0900

    프로그래머스 12948 핸드폰 번호 가리기

---
+++ src/main/java/Programmers/Programmers12948.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class Programmers12948 {
+    public String solution(String phone_number) {
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < phone_number.length(); i++){
+            if( i < phone_number.length()-4){
+                sb.append("*");
+            }else {
+                sb.append(phone_number.charAt(i));
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers12948 p = new Programmers12948();
+        System.out.println(p.solution("01033334444"));
+        System.out.println(p.solution("027778888"));
+    }
+}

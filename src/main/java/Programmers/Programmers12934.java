45b163c25565a015b22f3cc1c2ddcb7e5388d347
Jeong-Yoon <yj4025@gmail.com>
2019-02-16 21:37:51 +0900

    프로그래머스 12934 정수 제곱근 판별

---
+++ src/main/java/Programmers/Programmers12934.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class Programmers12934 {
+    public long solution(long n) {
+        long answer = 0;
+        if(Math.sqrt(n)%1 == 0){
+            answer = (long)Math.pow(Math.sqrt(n)+1,2);
+        }else{
+            answer = -1;
+        }
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers12934 p = new Programmers12934();
+        System.out.println(p.solution(121));
+        System.out.println(p.solution(3));
+
+    }
+}

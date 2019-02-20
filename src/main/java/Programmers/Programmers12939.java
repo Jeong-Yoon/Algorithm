c6fcdf3722d5d70f697b9875bfdfde35ff321978
Jeong-Yoon <yj4025@gmail.com>
2019-02-20 10:40:31 +0900

    프로그래머스 12939 최댓값과 최솟값

---
+++ src/main/java/Programmers/Programmers12939.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class Programmers12939 {
+    public String solution(String s) {
+        String answer = "";
+        String[] num = s.split(" ");
+        int min = Integer.parseInt(num[0]);
+        int max = Integer.parseInt(num[0]);
+        for (int i = 1; i < num.length; i++){
+            min = Math.min(min,Integer.parseInt(num[i]));
+            max = Math.max(max,Integer.parseInt(num[i]));
+        }
+        answer = min + " " + max;
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers12939 p = new Programmers12939();
+        System.out.println(p.solution("1 2 3 4"));
+        System.out.println(p.solution("-1 -2 -3 -4"));
+        System.out.println(p.solution("-1 -1"));
+    }
+}

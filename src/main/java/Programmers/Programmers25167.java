d010ceec5773c47adf7b472b185a611f6c5825ab
Jeong-Yoon <yj4025@gmail.com>
2019-02-23 23:40:51 +0900

    no message

---
+++ src/main/java/Programmers/Programmers25167.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Programmers25167 {
+    public int solution(int[] people, int[] tshirts) {
+        int answer = 0;
+        Arrays.sort(people);
+        Arrays.sort(tshirts);
+        for (int i = 0; i < people.length; i++){
+            for (int j = 0; j < tshirts.length; j++){
+                if (people[i] <= tshirts[j]){
+                    answer++;
+                    tshirts[j] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String args[]){
+        Programmers25167 p = new Programmers25167();
+        int[] people = {2,3};
+        int[] tshirts = {1,2,3};
+        System.out.println(p.solution(people,tshirts));
+    }
+}

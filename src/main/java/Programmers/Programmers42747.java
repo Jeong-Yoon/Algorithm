c80809038e819222f08673910f5c251607f016a9
Jeong-Yoon <yj4025@gmail.com>
2019-03-05 17:11:14 +0900

    프로그래머스 42747 H-Index

---
+++ src/main/java/Programmers/Programmers42747.java
@@ -0,0 +1,32 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Programmers42747 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        int h = 0;
+        int x = citations[citations.length-1];
+        for (int i = 0; i <= x; i++) {
+            for (int j = 0; j < citations.length; j++) {
+                if (i <= citations[j]) {
+                    if (i <= citations.length - j) {
+                        answer = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        Programmers42747 p = new Programmers42747();
+//        int[] citations = {3, 0, 6, 1, 5};
+        int[] citations = {1, 2, 3, 3, 3, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9, 9, 10, 10, 10};
+        System.out.println(p.solution(citations));
+    }
+}

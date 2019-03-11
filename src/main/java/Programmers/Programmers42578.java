c1e3d56bd13a4ab233d0a1b34b0ba97cd9ec17ef
Jeong-Yoon <yj4025@gmail.com>
2019-03-11 21:19:03 +0900

    프로그래머스 42578 위장

---
+++ src/main/java/Programmers/Programmers42578.java
@@ -0,0 +1,31 @@
+package Programmers;
+
+import java.util.*;
+
+public class Programmers42578 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++){
+            if (map.containsKey(clothes[i][1])){
+                map.replace(clothes[i][1],map.get(clothes[i][1]) + 1);
+            }else {
+                map.put(clothes[i][1],1);
+            }
+        }
+
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()){
+            answer *= map.get(iterator.next()) + 1;
+        }
+
+
+        return answer - 1;
+    }
+
+    public static void main(String args[]){
+        Programmers42578 p = new Programmers42578();
+        String[][] clothes = {{"yellow_hat","headgear"},{"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(p.solution(clothes));
+    }
+}

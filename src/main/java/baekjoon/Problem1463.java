dd8fd87c233b45ca3854dac87a5c0c2f8d052b95
Jeong-Yoon <yj4025@gmail.com>
2019-06-19 20:05:21 +0900

    백준 1463 1로 만들기

---
+++ src/main/java/baekjoon/Problem1463.java
@@ -0,0 +1,25 @@
+package baekjoon;
+/*
+1로 만들기
+ */
+import java.util.Scanner;
+
+public class Problem1463 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num+1];
+        arr[0] = arr[1] = 0;
+        for(int i = 2; i <= num; i++){
+            arr[i] = arr[i-1] + 1;
+            if(i%2 == 0){
+                arr[i] = Math.min(arr[i],arr[i/2]+1);
+            }
+            if(i%3 == 0){
+                arr[i] = Math.min(arr[i],arr[i/3]+1);
+            }
+        }
+        System.out.println(arr[num]);
+        sc.close();
+    }
+}

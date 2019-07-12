385088d015f8850465743a4c48ec153ca2b6f5e6
Jeong-Yoon <yj4025@gmail.com>
2019-07-12 22:20:58 +0900

    백준 11654 아스키 코드

---
+++ src/main/java/baekjoon/Problem11654.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+/*
+ * 아스키 코드
+ */
+import java.util.Scanner;
+
+public class Problem11654 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		System.out.println(c - 0);
+	}
+
+}
